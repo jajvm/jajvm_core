8ce80817cc900cffe2ffcaeb229c2c60e7da5e6d
getBoolean <danielwnewville@gmail.com>
2022-05-23 19:17:29 -0500

    Initial commit

---
+++ example/jajvm_core_example.dart
@@ -0,0 +1,6 @@
+import 'package:jajvm_core/jajvm_core.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/jajvm_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library jajvm_core;
+
+export 'src/jajvm_core_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
