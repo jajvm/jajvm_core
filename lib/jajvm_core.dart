9529f18cbe7cdef3eb62c96331fded0015a6c20b
getBoolean <danielwnewville@gmail.com>
2022-05-29 17:31:41 -0500

    export files

--- lib/jajvm_core.dart
+++ lib/jajvm_core.dart
@@ -3,6 +3,11 @@
 /// More dartdocs go here.
 library jajvm_core;
 
+export 'src/constants/env_vars.dart';
+export 'src/constants/error_codes.dart';
+export 'src/constants/supported_platform.dart';
+export 'src/exceptions/jajvm_exception.dart';
 export 'src/jajvm_application_controller.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/models/java_project.dart';
+export 'src/models/java_release.dart';
+export 'src/services/file_system_service.dart';
