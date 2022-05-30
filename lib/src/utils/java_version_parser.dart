10afef0824d3f95db63d709b42c1be55802b1087
getBoolean <danielwnewville@gmail.com>
2022-05-30 12:45:54 -0500

    simplify function

--- lib/src/utils/java_version_parser.dart
+++ lib/src/utils/java_version_parser.dart
@@ -9,18 +9,17 @@ class JavaVersionParser {
 
   JavaVersionParser(this.data);
 
-  List<MapEntry<String, String>> get javaVersionProperties {
-    final lines = data.split('\n');
-    return lines
-        .map((line) =>
-            line.trim().split('=').map((part) => part.trim()).toList())
-        .fold<List<MapEntry<String, String>>>(
-            [],
-            (List<MapEntry<String, String>> accumulator, List<String> line) => [
-                  ...accumulator,
-                  if (line.length == 2) ...[
-                    MapEntry(line[0], line[1].replaceAll('"', '')),
-                  ],
-                ]);
-  }
+  List<MapEntry<String, String>> get javaVersionProperties => data
+      .split('\n')
+      // Split the lines
+      .map((line) => line.trim().split('=').map((part) => part.trim()).toList())
+      // Convert to a list of [key, value]
+      .fold<List<MapEntry<String, String>>>(
+          [],
+          (List<MapEntry<String, String>> accumulator, List<String> line) => [
+                ...accumulator,
+                if (line.length == 2) ...[
+                  MapEntry(line[0], line[1].replaceAll('"', '')),
+                ],
+              ]);
 }
